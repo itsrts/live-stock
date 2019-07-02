4f93fbc61af6cb6c969c601c7366233e609f714d
Rajdeep <hi@rajdeepdeb.me>
2019-07-02 14:01:01 +0530

    Updates

---
+++ precache-manifest.7757595d1fe7c55f98b3feb27e5a97e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a8a6f4bfb5ada23cba0084a721054b5",
+    "url": "/live-stock/index.html"
+  },
+  {
+    "revision": "83a1b640e9f580bc98f5",
+    "url": "/live-stock/static/css/main.758e18ef.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/live-stock/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "83a1b640e9f580bc98f5",
+    "url": "/live-stock/static/js/main.5489d63a.chunk.js"
+  },
+  {
+    "revision": "f635c584b0e6ed23d826",
+    "url": "/live-stock/static/js/runtime~main.08c57171.js"
+  }
+]);
